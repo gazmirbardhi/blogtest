cb409ef1178bd4ebd24515514ebb1c3824b1978a
Arpit Goyal <arpit.goyal@mtxb2b.com>
2019-05-12 17:59:45 +0530

    add featured post

--- src/templates/home.js
+++ src/templates/home.js
@@ -6,8 +6,54 @@ import components, {Layout} from '../components/index';
 import safePrefix from '../utils/safePrefix';
 
 export default class Home extends React.Component {
+
+    getList(post, post_idx) {
+      return (
+        <article key={post_idx} className="post post-card">
+          <div className="post-card-inside">
+            {_.get(post, 'frontmatter.thumb_img_path') && 
+              (
+                _.get(post, 'frontmatter.is_external')
+                ?
+                <a target="_blank" rel="noopener noreferrer" className="post-card-thumbnail" href={safePrefix(_.get(post, 'frontmatter.url'))}>
+                  <img className="thumbnail" src={safePrefix(_.get(post, 'frontmatter.thumb_img_path'))} alt={_.get(post, 'frontmatter.title')} />
+                </a>
+                :
+                <a className="post-card-thumbnail" href={safePrefix(_.get(post, 'url'))}>
+                  <img className="thumbnail" src={safePrefix(_.get(post, 'frontmatter.thumb_img_path'))} alt={_.get(post, 'frontmatter.title')} />
+                </a>
+              )
+            }
+            <div className="post-card-content">
+              <header className="post-header">
+                <div className="post-meta">
+                  <time className="published"
+                  datetime={moment(_.get(post, 'frontmatter.date')).strftime('%Y-%m-%d %H:%M')}>{moment(_.get(post, 'frontmatter.date')).strftime('%B %d, %Y')}</time>
+                </div>
+                <h2 className="post-title"><a href={safePrefix(_.get(post, 'url'))} rel="bookmark">{_.get(post, 'frontmatter.title')}</a></h2>
+              </header>
+              <div className="post-excerpt">
+                <p>{_.get(post, 'frontmatter.excerpt')}</p>
+                <p className="read-more">
+                  {
+                    _.get(post, 'frontmatter.is_external')
+                    ?
+                    <a target="_blank" rel="noopener noreferrer" className="button inverse" href={safePrefix(_.get(post, 'frontmatter.url'))}>Read more</a>
+                    :
+                    <a className="button inverse" href={safePrefix(_.get(post, 'url'))}>Read more</a>
+                  }
+                </p>
+              </div>
+            </div>
+          </div>
+        </article>
+      )
+    }
+
     render() {
         let display_posts = _.orderBy(_.get(this.props, 'pageContext.pages').filter(page => page.relativeDir === 'posts'), 'frontmatter.date', 'desc');
+        const featured_posts = display_posts.filter(page => _.get(page, 'frontmatter.is_featured'))
+        const non_featured_posts = display_posts.filter(page => !_.get(page, 'frontmatter.is_featured'))
         return (
             <Layout {...this.props}>
               {_.map(_.get(this.props, 'pageContext.frontmatter.sections'), (section, section_idx) => {
@@ -17,45 +63,13 @@ export default class Home extends React.Component {
                   )
               })}
               <div className="post-feed">
-                {_.map(display_posts, (post, post_idx) => (
-                <article key={post_idx} className="post post-card">
-                  <div className="post-card-inside">
-                    {_.get(post, 'frontmatter.thumb_img_path') && 
-                      (
-                        _.get(post, 'frontmatter.is_external')
-                        ?
-                        <a target="_blank" rel="noopener noreferrer" className="post-card-thumbnail" href={safePrefix(_.get(post, 'frontmatter.url'))}>
-                          <img className="thumbnail" src={safePrefix(_.get(post, 'frontmatter.thumb_img_path'))} alt={_.get(post, 'frontmatter.title')} />
-                        </a>
-                        :
-                        <a className="post-card-thumbnail" href={safePrefix(_.get(post, 'url'))}>
-                          <img className="thumbnail" src={safePrefix(_.get(post, 'frontmatter.thumb_img_path'))} alt={_.get(post, 'frontmatter.title')} />
-                        </a>
-                      )
-                    }
-                    <div className="post-card-content">
-                      <header className="post-header">
-                        <div className="post-meta">
-                          <time className="published"
-                          datetime={moment(_.get(post, 'frontmatter.date')).strftime('%Y-%m-%d %H:%M')}>{moment(_.get(post, 'frontmatter.date')).strftime('%B %d, %Y')}</time>
-                        </div>
-                        <h2 className="post-title"><a href={safePrefix(_.get(post, 'url'))} rel="bookmark">{_.get(post, 'frontmatter.title')}</a></h2>
-                      </header>
-                      <div className="post-excerpt">
-                        <p>{_.get(post, 'frontmatter.excerpt')}</p>
-                        <p className="read-more">
-                          {
-                            _.get(post, 'frontmatter.is_external')
-                            ?
-                            <a target="_blank" rel="noopener noreferrer" className="button inverse" href={safePrefix(_.get(post, 'frontmatter.url'))}>Read more</a>
-                            :
-                            <a className="button inverse" href={safePrefix(_.get(post, 'url'))}>Read more</a>
-                          }
-                        </p>
-                      </div>
-                    </div>
-                  </div>
-                </article>
+                <div class="layout-list">
+                {_.map(featured_posts, (post, post_idx) => (
+                  this.getList(post, post_idx)
+                ))}
+                </div>
+                {_.map(non_featured_posts, (post, post_idx) => (
+                  this.getList(post, post_idx)
                 ))}
               </div>
             </Layout>
