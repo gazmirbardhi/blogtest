1b958b8403bef9f5379c37340bd5f0c609e748f7
Arpit Goyal <arpit.goyal@mtxb2b.com>
2019-05-16 14:17:23 +0530

    make arrow link too

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,26 +9,26 @@ export default class Header extends React.Component {
             <header id="masthead" className="site-header">
               <div className="go-back__nav is-hidden-mobile has-text-white">
                 <div className="go-back__wrapper">
-                  <div className="go-back">
-                      <span className="arrow-left">
-                      {/*<!-- css generated icon -->*/}
-                      </span>
-                      <a href="//arpitgoyal.com" className="back-title">Back</a>
-                    </div>
+                  <a href="//arpitgoyal.com" className="go-back">
+                    <span className="arrow-left">
+                    {/*<!-- css generated icon -->*/}
+                    </span>
+                    <span className="back-title">Back</span>
+                  </a>
                 </div>
               </div>
               {/*<div className="inner">
                 <div className="site-header-inside">
                   <div className="site-branding">
-                    {_.get(this.props, 'pageContext.site.siteMetadata.header.logo_img') && 
+                    {_.get(this.props, 'pageContext.site.siteMetadata.header.logo_img') &&
                     <p className="site-logo">
                       <a href={safePrefix(_.get(this.props, 'pageContext.site.pathPrefix') || '/')}><img src={safePrefix(_.get(this.props, 'pageContext.site.siteMetadata.header.logo_img'))}
                           alt="Logo" /></a>
                     </p>
                     }
-                    {(_.get(this.props, 'pageContext.frontmatter.template') === 'home') ? 
+                    {(_.get(this.props, 'pageContext.frontmatter.template') === 'home') ?
                     <h1 className="site-title"><a href={safePrefix(_.get(this.props, 'pageContext.site.pathPrefix') || '/')}>{_.get(this.props, 'pageContext.site.siteMetadata.header.title') ? _.get(this.props, 'pageContext.site.siteMetadata.header.title') : _.get(this.props, 'pageContext.site.data.author.name')}</a></h1>
-                     : 
+                     :
                     <p className="site-title"><a href={safePrefix(_.get(this.props, 'pageContext.site.pathPrefix') || '/')}>{_.get(this.props, 'pageContext.site.siteMetadata.header.title') ? _.get(this.props, 'pageContext.site.siteMetadata.header.title') : _.get(this.props, 'pageContext.site.data.author.name')}</a></p>
                     }
                   </div>
