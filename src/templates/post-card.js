083803868b1bbe1dd6cae4a10234104544dc7c4a
Arpit Goyal <arpit.goyal@mtxb2b.com>
2019-05-18 08:31:03 +0530

    add more attr for gatsby-image

--- src/templates/post-card.js
+++ src/templates/post-card.js
@@ -1,70 +1,121 @@
-import React from 'react';
-import { graphql } from "gatsby"
-import Img from "gatsby-image"
-import _ from 'lodash';
-import moment from 'moment-strftime';
-import safePrefix from '../utils/safePrefix';
+import React from "react";
+import { graphql } from "gatsby";
+import Img from "gatsby-image";
+import _ from "lodash";
+import moment from "moment-strftime";
+import safePrefix from "../utils/safePrefix";
 
-const PostCard = (props) => {
-	const {post, data} = props;
-  const fluid = _.get(data, 'file.childImageSharp.fluid');
+const PostCard = props => {
+  const { post, data } = props;
+  const fluid = _.get(data, "file.childImageSharp.fluid");
   // console.log("props => ", props);
-	// console.log("fluid => ", fluid);
-	return (
-  <article className="post post-card">
-    <div className="post-card-inside">
-      {_.get(post, 'frontmatter.thumb_img_path') && 
-        (
-          _.get(post, 'frontmatter.is_external')
-          ?
-          <a target="_blank" rel="noopener noreferrer" className="post-card-thumbnail" href={safePrefix(_.get(post, 'frontmatter.url'))}>
-            <Img fluid={fluid ? fluid : {"src": "/static/4cac8be2ce5d1ce879b6320fb2765137/cf658/1-ssPaHnqjgTjkP0hwO8nGZw.jpg", "aspectRatio": 2.8426395939086295}} />
-            {/*<img className="thumbnail" src={safePrefix(_.get(post, 'frontmatter.thumb_img_path'))} alt={_.get(post, 'frontmatter.title')} />*/}
-          </a>
-          :
-          <a className="post-card-thumbnail" href={safePrefix(_.get(post, 'url'))}>
-            <Img fluid={fluid ? fluid : {"src": "/static/4cac8be2ce5d1ce879b6320fb2765137/cf658/1-ssPaHnqjgTjkP0hwO8nGZw.jpg", "aspectRatio": 2.8426395939086295}} />
-            {/*<img className="thumbnail" src={safePrefix(_.get(post, 'frontmatter.thumb_img_path'))} alt={_.get(post, 'frontmatter.title')} />*/}
-          </a>
-        )
-      }
-      <div className="post-card-content">
-        <header className="post-header">
-          <div className="post-meta">
-            <time className="published"
-            dateTime={moment(_.get(post, 'frontmatter.date')).strftime('%Y-%m-%d %H:%M')}>{moment(_.get(post, 'frontmatter.date')).strftime('%B %d, %Y')}</time>
+  // console.log("fluid => ", fluid);
+  return (
+    <article className="post post-card">
+      <div className="post-card-inside">
+        {_.get(post, "frontmatter.thumb_img_path") &&
+          (_.get(post, "frontmatter.is_external") ? (
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              className="post-card-thumbnail"
+              href={safePrefix(_.get(post, "frontmatter.url"))}
+            >
+              <Img
+                fluid={
+                  fluid
+                    ? fluid
+                    : {
+                        src:
+                          "/static/4cac8be2ce5d1ce879b6320fb2765137/cf658/1-ssPaHnqjgTjkP0hwO8nGZw.jpg",
+                        aspectRatio: 2.8426395939086295,
+                        srcSet:
+                          "/static/4cac8be2ce5d1ce879b6320fb2765137/5398f/1-ssPaHnqjgTjkP0hwO8nGZw.jpg 200w,\n/static/4cac8be2ce5d1ce879b6320fb2765137/26a4d/1-ssPaHnqjgTjkP0hwO8nGZw.jpg 400w,\n/static/4cac8be2ce5d1ce879b6320fb2765137/cf658/1-ssPaHnqjgTjkP0hwO8nGZw.jpg 560w",
+                        sizes: "(max-width: 560px) 100vw, 560px"
+                      }
+                }
+              />
+              {/*<img className="thumbnail" src={safePrefix(_.get(post, 'frontmatter.thumb_img_path'))} alt={_.get(post, 'frontmatter.title')} />*/}
+            </a>
+          ) : (
+            <a
+              className="post-card-thumbnail"
+              href={safePrefix(_.get(post, "url"))}
+            >
+              <Img
+                fluid={
+                  fluid
+                    ? fluid
+                    : {
+                        src:
+                          "/static/4cac8be2ce5d1ce879b6320fb2765137/cf658/1-ssPaHnqjgTjkP0hwO8nGZw.jpg",
+                        aspectRatio: 2.8426395939086295,
+                        srcSet:
+                          "/static/4cac8be2ce5d1ce879b6320fb2765137/5398f/1-ssPaHnqjgTjkP0hwO8nGZw.jpg 200w,\n/static/4cac8be2ce5d1ce879b6320fb2765137/26a4d/1-ssPaHnqjgTjkP0hwO8nGZw.jpg 400w,\n/static/4cac8be2ce5d1ce879b6320fb2765137/cf658/1-ssPaHnqjgTjkP0hwO8nGZw.jpg 560w",
+                        sizes: "(max-width: 560px) 100vw, 560px"
+                      }
+                }
+              />
+              {/*<img className="thumbnail" src={safePrefix(_.get(post, 'frontmatter.thumb_img_path'))} alt={_.get(post, 'frontmatter.title')} />*/}
+            </a>
+          ))}
+        <div className="post-card-content">
+          <header className="post-header">
+            <div className="post-meta">
+              <time
+                className="published"
+                dateTime={moment(_.get(post, "frontmatter.date")).strftime(
+                  "%Y-%m-%d %H:%M"
+                )}
+              >
+                {moment(_.get(post, "frontmatter.date")).strftime("%B %d, %Y")}
+              </time>
+            </div>
+            <h2 className="post-title">
+              <a href={safePrefix(_.get(post, "url"))} rel="bookmark">
+                {_.get(post, "frontmatter.title")}
+              </a>
+            </h2>
+          </header>
+          <div className="post-excerpt">
+            <p>{_.get(post, "frontmatter.excerpt")}</p>
+            <p className="read-more">
+              {_.get(post, "frontmatter.is_external") ? (
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="button inverse"
+                  href={safePrefix(_.get(post, "frontmatter.url"))}
+                >
+                  Read more
+                </a>
+              ) : (
+                <a
+                  className="button inverse"
+                  href={safePrefix(_.get(post, "url"))}
+                >
+                  Read more
+                </a>
+              )}
+            </p>
           </div>
-          <h2 className="post-title"><a href={safePrefix(_.get(post, 'url'))} rel="bookmark">{_.get(post, 'frontmatter.title')}</a></h2>
-        </header>
-        <div className="post-excerpt">
-          <p>{_.get(post, 'frontmatter.excerpt')}</p>
-          <p className="read-more">
-            {
-              _.get(post, 'frontmatter.is_external')
-              ?
-              <a target="_blank" rel="noopener noreferrer" className="button inverse" href={safePrefix(_.get(post, 'frontmatter.url'))}>Read more</a>
-              :
-              <a className="button inverse" href={safePrefix(_.get(post, 'url'))}>Read more</a>
-            }
-          </p>
         </div>
       </div>
-    </div>
-  </article>
-)
-}
+    </article>
+  );
+};
 
-export default PostCard
+export default PostCard;
 
 export const pageQuery = graphql`
   query {
-    file(relativePath: {eq: "1-ssPaHnqjgTjkP0hwO8nGZw.jpg"}) {
+    file(relativePath: { eq: "1-ssPaHnqjgTjkP0hwO8nGZw.jpg" }) {
       childImageSharp {
         fluid(maxWidth: 800) {
-         src 
+          src
         }
       }
     }
   }
-`
+`;
 //...GatsbyImageSharpFluid
