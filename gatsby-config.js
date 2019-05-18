3a783a5db6a175aa858427d4233a0f0396be8f30
Arpit Goyal <arpit.goyal@mtxb2b.com>
2019-05-18 08:22:43 +0530

    add gatsby-image plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,6 +30,13 @@ module.exports = {
         {
             resolve: `gatsby-source-filesystem`,
             options: {
+                name: `images`,
+                path: `${__dirname}/static/images`,
+            },
+        },
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
                 name: `pages`,
                 path: `${__dirname}/src/pages`,
             },
