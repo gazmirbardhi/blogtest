a7fde3a49f3f7adb20789e4e1f05b610bd86f4d9
Arpit Goyal <arpit.goyal@mtxb2b.com>
2019-07-04 01:39:25 +0530

    add product portfolio screen #2

--- src/templates/product-portfolio.js
+++ src/templates/product-portfolio.js
@@ -45,7 +45,7 @@ export default class ProductPortfolio extends React.Component {
     }
 
     render() {
-        let display_posts = _.orderBy(_.get(this.props, 'pageContext.pages').filter(page => page.relativeDir === 'posts'), 'frontmatter.date', 'desc');
+        let display_posts = _.orderBy(_.get(this.props, 'pageContext.pages').filter(page => page.relativeDir === 'products'), 'frontmatter.date', 'desc');
         const featured_posts = display_posts.filter(page => _.get(page, 'frontmatter.is_featured'))
         const non_featured_posts = display_posts.filter(page => !_.get(page, 'frontmatter.is_featured'))
         return (
