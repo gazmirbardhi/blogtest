bd8c967a9540acbb48386c2aeceed7d16824ae86
Arpit Goyal <arpit.goyal@mtxb2b.com>
2019-07-12 23:55:38 +0530

    update home page

--- src/templates/home.js
+++ src/templates/home.js
@@ -12,7 +12,7 @@ export default class Notfound extends React.Component {
                 <div className="has-bg is-home">
                     <main className="image-container">
                         <img src={safePrefix(_.get(this.props, 'pageContext.frontmatter.illustration_image' ))} alt="Illustration" />
-                        <section className="section hero is-fullheight">
+                        <section className="section hero is-fullheight is-paddingless">
                             <div className="hero-body has-text-white">
                                 <div className="container is-fluid">
                                     <div className="columns">
@@ -20,7 +20,8 @@ export default class Notfound extends React.Component {
                                         <div className="column is-three-quarters">
                                             <section className="section">
                                                 <h1 className="title has-text-white">Arpit Goyal</h1>
-                                                <h2 className="subtitle is-size-4 has-text-white">
+                                                <span></span>
+                                                <h2 className="subtitle is-size-4 has-text-white is-marginless">
                                                     Product Manager | Engineer with an eye for design
                                                 </h2>
                                             </section>
@@ -30,7 +31,7 @@ export default class Notfound extends React.Component {
                                                         <a href="/work-portfolio" className="tile is-child box things-i-do">
                                                             <img src={safePrefix(_.get(this.props, 'pageContext.frontmatter.build_image' ))} alt="Build Products" />
                                                             <p className="title">Build</p>
-                                                            <p>
+                                                            <p className="is-marginless">
                                                                 I build products.<br />Code &amp; design.
                                                             </p>
                                                         </a>
@@ -39,7 +40,7 @@ export default class Notfound extends React.Component {
                                                         <a href="/blog" className="tile is-child box things-i-do">
                                                             <img src={safePrefix(_.get(this.props, 'pageContext.frontmatter.blog_image' ))} alt="Blogging" />
                                                             <p className="title">Blog</p>
-                                                            <p>
+                                                            <p className="is-marginless">
                                                                 I write, and<br />share my opinions
                                                             </p>
                                                         </a>
@@ -48,36 +49,36 @@ export default class Notfound extends React.Component {
                                                         <div className="tile is-child box things-i-do coming-soon">
                                                             <img src={safePrefix(_.get(this.props, 'pageContext.frontmatter.everything_else_image' ))} alt="Everything else" />
                                                             <p className="title three" />
-                                                            <p>
+                                                            <p className="is-marginless">
                                                                 Travelling, Cycling,<br />everything else
                                                             </p>
                                                         </div>
                                                     </div>
                                                 </div>
                                                 <div>
-                                                    <a href="/resume" target="_blank" rel="noopener noreferrer" className="is-link negative-margin-top has-text-white level is-flex-mobile level-left is-inline-flex">
+                                                    <a href="/resume" target="_blank" rel="noopener noreferrer" className="is-link is-borderless negative-margin-top has-text-white level is-flex-mobile level-left is-inline-flex">
                                                         <span className="c">look at my résumé here</span>
                                                     </a>
                                                 </div>
                                             </section>
                                             <section className="section social">
                                                 <div className="level">
-                                                    <a href="https://medium.com/@arpit_goyal" target="_blank" rel="noopener noreferrer" className="is-link  negative-margin-top has-text-white level is-flex-mobile is-inline-flex level-left">
-                                                        <img src={safePrefix(_.get(this.props, 'pageContext.frontmatter.medium_icon' ))} alt="medium" />
+                                                    <a href="https://medium.com/@arpit_goyal" target="_blank" rel="noopener noreferrer" className="is-link is-borderless  negative-margin-top has-text-white level is-flex-mobile is-inline-flex level-left">
+                                                        <img className="i" src={safePrefix(_.get(this.props, 'pageContext.frontmatter.medium_icon' ))} alt="medium" />
                                                         <span className="c">
                                                             I've written some articles on Medium
                                                         </span>
                                                     </a>
                                                 </div>
                                                 <div className="level">
-                                                    <a href="https://twitter.com/_arpitgoyal" target="_blank" rel="noopener noreferrer" className="is-link  negative-margin-top has-text-white level is-flex-mobile is-inline-flex level-left">
-                                                        <img src={safePrefix(_.get(this.props, 'pageContext.frontmatter.twitter_icon' ))} alt="twitter" />
+                                                    <a href="https://twitter.com/_arpitgoyal" target="_blank" rel="noopener noreferrer" className="is-link is-borderless  negative-margin-top has-text-white level is-flex-mobile is-inline-flex level-left">
+                                                        <img className="i" src={safePrefix(_.get(this.props, 'pageContext.frontmatter.twitter_icon' ))} alt="twitter" />
                                                         <span className="c">I tweet, sometimes</span>
                                                     </a>
                                                 </div>
                                                 <div className="level">
-                                                    <a href="https://www.quora.com/profile/Arpit-Goyal-14" target="_blank" rel="noopener noreferrer" className="is-link  negative-margin-top has-text-white is-flex-mobile is-inline-flex level level-left">
-                                                        <img src={safePrefix(_.get(this.props, 'pageContext.frontmatter.quora_icon' ))} alt="quora" />
+                                                    <a href="https://www.quora.com/profile/Arpit-Goyal-14" target="_blank" rel="noopener noreferrer" className="is-link is-borderless  negative-margin-top has-text-white is-flex-mobile is-inline-flex level level-left">
+                                                        <img className="i" src={safePrefix(_.get(this.props, 'pageContext.frontmatter.quora_icon' ))} alt="quora" />
                                                         <span className="c">Some answers on Quora</span>
                                                     </a>
                                                 </div>
