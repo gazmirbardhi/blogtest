1557131c33725e873b7ba8f4242aeb67a984cd40
Arpit Goyal <arpit.goyal@mtxb2b.com>
2019-07-13 01:59:51 +0530

    update home template #2

--- src/pages/index.md
+++ src/pages/index.md
@@ -6,13 +6,6 @@ menus:
     title: Home
     weight: 1
 template: home
-build_image: https://res.cloudinary.com/arpit-goyal/image/upload/v1562861242/code.png
-blog_image: https://res.cloudinary.com/arpit-goyal/image/upload/v1562861242/write.png
-everything_else_image: https://res.cloudinary.com/arpit-goyal/image/upload/v1562861242/everything.png
-illustration_image: https://res.cloudinary.com/arpit-goyal/image/upload/v1562861242/arpit-goyal.png
-quora_icon: https://res.cloudinary.com/arpit-goyal/image/upload/v1562864624/quora.svg
-medium_icon: https://res.cloudinary.com/arpit-goyal/image/upload/v1562864636/medium.svg
-twitter_icon: https://res.cloudinary.com/arpit-goyal/image/upload/v1562864645/twitter.svg
 keywords: '"arpit", "goyal", "mumbai", "Jaipur", "Product Manager", "UX designer",
   "india", "developer", "frontend", "arpit goyal", "goyalarpit", "arpitgoyal"'
 excerpt: Hi, I'm Arpit Goyal, Engineer with an eye for design. Startup guy, UX consultant
@@ -20,10 +13,11 @@ excerpt: Hi, I'm Arpit Goyal, Engineer with an eye for design. Startup guy, UX c
   products and making money.
 header: Arpit Goyal
 secondary_header: Product Manager | Engineer with an eye for design
+illustration_image: https://res.cloudinary.com/arpit-goyal/image/upload/v1562861242/arpit-goyal.png
 social_icons:
-- https://res.cloudinary.com/arpit-goyal/image/upload/v1562868460/quora.svg
 - https://res.cloudinary.com/arpit-goyal/image/upload/v1562868460/medium.svg
 - https://res.cloudinary.com/arpit-goyal/image/upload/v1562868460/twitter.svg
+- https://res.cloudinary.com/arpit-goyal/image/upload/v1562868460/quora.svg
 things_i_do_illustrations:
 - https://res.cloudinary.com/arpit-goyal/image/upload/v1562861242/code.png
 - https://res.cloudinary.com/arpit-goyal/image/upload/v1562861242/write.png
--- src/templates/home.js
+++ src/templates/home.js
@@ -29,7 +29,7 @@ export default class Notfound extends React.Component {
                                                 <div className="tile is-ancestor">
                                                     <div className="tile is-horizontal is-parent">
                                                         <a href="/work-portfolio" className="tile is-child box things-i-do">
-                                                            <img src={safePrefix(_.get(this.props, 'pageContext.frontmatter.build_image' ))} alt="Build Products" />
+                                                            <img src={safePrefix(_.get(this.props, 'pageContext.frontmatter.things_i_do_illustrations[0]' ))} alt="Build Products" />
                                                             <p className="title">Build</p>
                                                             <p className="is-marginless">
                                                                 I build products.<br />Code &amp; design.
@@ -38,7 +38,7 @@ export default class Notfound extends React.Component {
                                                     </div>
                                                     <div className="tile is-horizontal is-parent">
                                                         <a href="/blog" className="tile is-child box things-i-do">
-                                                            <img src={safePrefix(_.get(this.props, 'pageContext.frontmatter.blog_image' ))} alt="Blogging" />
+                                                            <img src={safePrefix(_.get(this.props, 'pageContext.frontmatter.things_i_do_illustrations[1]' ))} alt="Blogging" />
                                                             <p className="title">Blog</p>
                                                             <p className="is-marginless">
                                                                 I write, and<br />share my opinions
@@ -47,7 +47,7 @@ export default class Notfound extends React.Component {
                                                     </div>
                                                     <div className="tile is-horizontal is-parent is-hidden-mobile">
                                                         <div className="tile is-child box things-i-do coming-soon">
-                                                            <img src={safePrefix(_.get(this.props, 'pageContext.frontmatter.everything_else_image' ))} alt="Everything else" />
+                                                            <img src={safePrefix(_.get(this.props, 'pageContext.frontmatter.things_i_do_illustrations[2]' ))} alt="Everything else" />
                                                             <p className="title three" />
                                                             <p className="is-marginless">
                                                                 Travelling, Cycling,<br />everything else
@@ -64,7 +64,7 @@ export default class Notfound extends React.Component {
                                             <section className="section social">
                                                 <div className="level">
                                                     <a href="https://medium.com/@arpit_goyal" target="_blank" rel="noopener noreferrer" className="is-link is-borderless  negative-margin-top has-text-white level is-flex-mobile is-inline-flex level-left">
-                                                        <img className="i" src={safePrefix(_.get(this.props, 'pageContext.frontmatter.medium_icon' ))} alt="medium" />
+                                                        <img className="i" src={safePrefix(_.get(this.props, 'pageContext.frontmatter.social_icons[0]' ))} alt="medium" />
                                                         <span className="c">
                                                             I've written some articles on Medium
                                                         </span>
@@ -72,13 +72,13 @@ export default class Notfound extends React.Component {
                                                 </div>
                                                 <div className="level">
                                                     <a href="https://twitter.com/_arpitgoyal" target="_blank" rel="noopener noreferrer" className="is-link is-borderless  negative-margin-top has-text-white level is-flex-mobile is-inline-flex level-left">
-                                                        <img className="i" src={safePrefix(_.get(this.props, 'pageContext.frontmatter.twitter_icon' ))} alt="twitter" />
+                                                        <img className="i" src={safePrefix(_.get(this.props, 'pageContext.frontmatter.social_icons[1]' ))} alt="twitter" />
                                                         <span className="c">I tweet, sometimes</span>
                                                     </a>
                                                 </div>
                                                 <div className="level">
                                                     <a href="https://www.quora.com/profile/Arpit-Goyal-14" target="_blank" rel="noopener noreferrer" className="is-link is-borderless  negative-margin-top has-text-white is-flex-mobile is-inline-flex level level-left">
-                                                        <img className="i" src={safePrefix(_.get(this.props, 'pageContext.frontmatter.quora_icon' ))} alt="quora" />
+                                                        <img className="i" src={safePrefix(_.get(this.props, 'pageContext.frontmatter.social_icons[2]' ))} alt="quora" />
                                                         <span className="c">Some answers on Quora</span>
                                                     </a>
                                                 </div>
