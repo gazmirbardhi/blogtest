aab79f9021880cb3a48fcb066b779f4fa82befea
Arpit Goyal <arpit.goyal@mtxb2b.com>
2019-07-13 09:20:20 +0530

    update google analytics id

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,7 +10,7 @@ module.exports = {
         {
             resolve: `gatsby-plugin-google-analytics`,
             options: {
-                trackingId: "UA-52772725-3",
+                trackingId: "UA-143748967-1",
             }
         },
         {
