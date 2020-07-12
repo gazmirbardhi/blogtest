47bb5c09af7008cdebecf62a797fbd8a3707bf14
Arpit Goyal <arpit.goyal@mtxb2b.com>
2020-07-12 23:15:43 +0530

    update photography layout #18

--- src/templates/photography.js
+++ src/templates/photography.js
@@ -10,8 +10,8 @@ export default class Page extends React.Component {
         return (
             <LayoutThree {...this.props}>
               <article className="post page post-full">
-                <header className="post-header is-marginless">
-                  <h1 className="post-title pl-6">{_.get(this.props, 'pageContext.frontmatter.title')}</h1>
+                <header className="post-header">
+                  <h1 className="post-title has-text-centered">{_.get(this.props, 'pageContext.frontmatter.title')}</h1>
                   {_.get(this.props, 'pageContext.frontmatter.subtitle') && 
                   <div className="post-subtitle">
                     {htmlToReact(_.get(this.props, 'pageContext.frontmatter.subtitle'))}
@@ -27,8 +27,10 @@ export default class Page extends React.Component {
                   {htmlToReact(_.get(this.props, 'pageContext.html'))}
                 </div>
                 <div className="columns bandw_gallery">
-                  <div className="column">
-                    <h2>My trial on B&W Photography:</h2>
+                  <div className="column pr-6">
+                    <h2 className="pt-6">My trials on B&W Photography:</h2>
+                    <p>We have to admit there is something exceptionally aesthetically pleasing in a well-composed black and white imagery where everything is stripped down to the core – light, contrast, textures, even emotions.</p>
+                    <p><i>There is definitely something elemental in black and white, which eliminates so many of the potential distractions that color is all about. Black and white can reduce a scene to something more easily and quickly absorbed. It retains a kind of purity which we respond to without so much study</i>, claims David Burnett, a legendary world-traveling photojournalist.</p>
                   </div>
                   <div className="column">
                     <div className="grid-container">
@@ -45,8 +47,11 @@ export default class Page extends React.Component {
                   </div>
                 </div>
                 <div className="columns coloured_gallery">
-                  <div className="column">
+                  <div className="column pr-6">
                     <h2 className="pt-6">Colored Photography:</h2>
+                    <p>When photographing the world around us, the property of color is likely something most people tend to take for granted. We expect our cameras to portray the visible light spectrum accurately. However, in a world so engrossed with color, we sometimes forget how long it took to get to this point in time and how many photographers and scientists viewed the concept as a pipe dream.</p>
+                    <p>When photography was invented in 1839, it was a black-and-white medium, and it remained that way for almost one hundred years.</p>
+                    <p></p>
                   </div>
                   <div className="column">
                     <div className="grid-container">
